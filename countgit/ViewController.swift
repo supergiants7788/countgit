18b1264a9767c356a883b457bc09c6eda0fddf61
新井山詠斗 <MAC-GIANTSPRIDE@araisaneito-no-macbook-air.local>
2016-05-25 19:28:20 +0900

    Initial Commit

---
+++ countgit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  countgit
+//
+//  Created by 新井山詠斗 on 2016/05/25.
+//  Copyright © 2016年 新井山詠斗. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
